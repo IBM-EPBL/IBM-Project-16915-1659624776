f7a77d0cb971de6ed9fde9a695244383e5a60fab
Sanofer Rasheed <67732935+san-rasheed@users.noreply.github.com>
2022-10-07 22:22:35 +0530

    Update main.py

--- Assignments/Team Lead- Sanofer Fathima/Assignment 3/main.py
+++ Assignments/Team Lead- Sanofer Fathima/Assignment 3/main.py
@@ -4,6 +4,18 @@ app= Flask(__name__)
 @app.route("/chatbot")
 def chat():
     return render_template("chatbot.html")
-  
-  if(__name__=='__main__'):
+
+@app.route("/assign3")
+def objstorage():
+ for i in range(0,len(imagearr)):
+ imagearr[i]="https://s3.jp-tok.cloud-objectstorage.appdomain.cloud/sanrasheed29/"+imagearr[i]
+ print("objstorage ", imagearr)
+ return render_template('assign3.html',arr=imagearr)
+
+@app.route('/redirect_to')
+def redirect_to():
+ link = request.args.get('link', '/')
+ return redirect(link), 301
+
+if(__name__=='__main__'):
     app.run()
